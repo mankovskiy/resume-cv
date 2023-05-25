fa8f7bf8fd563ab76cbd98110a15692d181fe7cd
mankovskiy <35701366+mankovskiy@users.noreply.github.com>
2023-05-25 22:28:02 +0000

    Deploying to gh-pages from @ mankovskiy/resume-cv@caeb9b9f15296479c9e911aa1907e897b7f11063 🚀

--- src/Page/Contacts/Contacts.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const SectionCont = styled.section`
-  padding-top: 70px;
-  padding-bottom: 250px;
-`;
--- src/Page/Skills/Skills.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const SectionSkills = styled.section`
-  padding-top: 70px;
-  padding-bottom: 360px;
-`;
